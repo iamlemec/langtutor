edd546a2ad23abd0e6a9e2724f86bfe3e30830cd
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-12-30 01:06:59 +0900

    working mockup

---
+++ react/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect, useRef } from 'react'
+import './App.css'
+
+const test = [
+  ['Hello world', 'Hello world'],
+  ['This is a test', 'This is a test'],
+]
+
+function App() {
+  const [article, setArticle] = useState(null)
+  const [chunks, setChunks] = useState(test)
+  const [cursor, setCursor] = useState(-1)
+  const inputRef = useRef(null)
+
+  function handleTranslate() {
+    const text = inputRef.current.value
+    if (text.length == 0) return
+    setArticle(text)
+    setCursor(-1)
+    setChunks(cs => [...cs, ...test])
+  }
+
+  // add keybindings
+  useEffect(() => {
+    function handleKeyDown(e) {
+      console.log(e.key)
+      if (e.key === 'ArrowDown') {
+        setCursor(c => Math.min(c + 1, chunks.length))
+      } else if (e.key === 'ArrowUp') {
+        setCursor(c => Math.max(-1, c - 1))
+      }
+    }
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [chunks])
+
+  return (
+    <div className="flex flex-row h-screen w-screen">
+      <div className="flex flex-col flex-1 h-full">
+        <div className="relative flex flex-row border-b border-gray-200">
+          <input type="text" className="w-full p-2 outline-none font-mono bg-gray-100" ref={inputRef} />
+          <div className="absolute right-0 h-full p-2">
+            <button className="px-1 bg-blue-500 text-white h-full w-full rounded-sm font-bold text-sm" onClick={handleTranslate}>Translate</button>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 w-full gap-2 p-2">
+          {chunks.map(([orig, trans], index) => (
+            <div key={index} className={`flex flex-row p-2 w-full border rounded-sm border-gray-200 ${cursor === index ? 'border-blue-500' : ''}`}>
+              <div className="w-[50%] mr-1 border-r border-gray-200">{orig}</div>
+              <div className="w-[50%] ml-1">
+                <span className={index > cursor ? 'bg-gray-100 text-gray-100' : ''}>{trans}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-[400px] h-full border-l border-gray-200 p-2">{article}</div>
+    </div>
+  )
+}
+
+export default App
